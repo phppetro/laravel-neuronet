53d88ee804ee6b1ba586ac12fafc68b1ae9cf0e3
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2019-06-28 17:05:04 +0200

    Reorganise modules in the dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,13 +11,13 @@
       	</div>
     		<div class="col-md-4">
     			@include('admin.dashboard2.metrics4')
-    			@include('admin.dashboard2.schedule2')
-    			@include('admin.dashboard2.publications2')
+					@include('admin.dashboard2.publications2')
+					@include('admin.dashboard2.documents2')
     		</div>
     		<div class="col-md-4">
+					@include('admin.dashboard2.schedule2')
     			@include('admin.dashboard2.calendar2')
     			@include('admin.dashboard2.deliverables3')
-					@include('admin.dashboard2.documents2')
         </div>
      </div>
   </div>
