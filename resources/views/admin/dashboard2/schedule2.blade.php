500650f34c27b0e737ccaf9a190eaf978960855b
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2019-06-28 16:53:52 +0200

    Added js chart the schedule module in the dashboard according to document NEURONET INKB Guide_200619

--- resources/views/admin/dashboard2/schedule2.blade.php
+++ resources/views/admin/dashboard2/schedule2.blade.php
@@ -1,6 +1,6 @@
             <div class="box box-indigo">
                 <div class="box-header with-border">
-                  <h3 class="box-title">Schedule <span class="small text-muted">(Sample gantt chart)</span></h3>
+                  <h3 class="box-title">Schedule</h3>
 
                   <div class="box-tools pull-right">
                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -10,11 +10,62 @@
                 </div>
                 <!-- /.box-header -->
                 <div class="box-body">
-                   <img src="https://pi.synapse-managers.com/img/gantt.png" class="img-thumbnail" alt="Responsive image">
+                  <div id="timeline" ></div>
                 </div>
                 <!-- /.box-body -->
-                  <div class="box-footer text-center">
-                    <a href="#" class="uppercase">View Schedule</a>
-                  </div>
-                <!-- /.box-footer -->
             </div>
+
+            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script>
+              google.charts.load('current', {'packages':['timeline']});
+              google.charts.setOnLoadCallback(drawChart);
+              function drawChart() {
+              var container = document.getElementById('timeline');
+              var chart = new google.visualization.Timeline(container);
+              var dataTable = new google.visualization.DataTable();
+
+              dataTable.addColumn({ type: 'string', id: 'Role' });
+              dataTable.addColumn({ type: 'string', id: 'Name' });
+              dataTable.addColumn({ type: 'date', id: 'Start' });
+              dataTable.addColumn({ type: 'date', id: 'End' });
+              dataTable.addRows([
+                [ '1', 'ADAPTED', new Date(2016, 9, 30), new Date(2019, 9, 30) ],
+                [ '2', 'AETIONOMY', new Date(2014, 1, 1), new Date(2018, 12, 31) ],
+                [ '3', 'AMYPAD', new Date(2016, 9, 30), new Date(2021, 9, 30) ],
+                [ '4', 'EMIF-AD', new Date(2013, 1, 1), new Date(2018, 6, 30) ],
+                [ '5', 'EPAD', new Date(2015, 1, 1), new Date(2019, 12, 31) ],
+                [ '6', 'EQIPD', new Date(2017, 9, 30), new Date(2020, 9, 30) ],
+                [ '7', 'IM2PACT', new Date(2019, 1, 1), new Date(2023, 12, 31) ],
+                [ '8', 'IMPRIND', new Date(2017, 1, 1), new Date(2021, 9, 30) ],
+                [ '9', 'MOPEAD', new Date(2016, 9, 30), new Date(2019, 9, 30) ],
+                [ '10', 'PD-MITOQUANT', new Date(2019, 1, 1), new Date(2021, 12, 31) ],
+                [ '11', 'PHAGO', new Date(2016, 9, 30), new Date(2021, 9, 30) ],
+                [ '12', 'PRISM', new Date(2016, 3, 30), new Date(2019, 12, 31) ],
+                [ '13', 'RADAR-CNS', new Date(2016, 3, 30), new Date(2021, 3, 30) ],
+                [ '14', 'RADAR-AD', new Date(2019, 1, 1), new Date(2022, 6, 30) ],
+                [ '15', 'ROADMAP', new Date(2016, 9, 30), new Date(2018, 12, 31) ]]);
+
+                var options = {
+                  title: 'Rate the Day on a Scale of 1 to 10',
+                  height: 614,
+                  hAxis: {
+                    format: 'yyyy',
+                    title: 'time',
+                    gridlines: {count: 4}
+                  },
+                  timeline: {
+                    showBarLabels: true,
+                    showRowLabels: false,
+                    rowLabelStyle: { fontSize: 11 },
+                    barLabelStyle: { fontSize:11 }
+                  },
+                  tooltip: {
+                    trigger: focus //none
+                  },
+                  colors: ['#3a0961', '#9b6deb'],
+                  backgroundColor: '#fff'
+                };
+
+              chart.draw(dataTable, options);
+              }
+            </script>
