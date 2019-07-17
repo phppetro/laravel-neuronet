7f1dc519471300a4b32eb023b4a4d1c0048903c3
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2019-07-17 16:28:20 +0200

    Changed the look in the Document module in the dashboard

--- resources/views/admin/dashboard2/documents2.blade.php
+++ resources/views/admin/dashboard2/documents2.blade.php
@@ -10,21 +10,30 @@
                 </div>
                 <!-- /.box-header -->
                <div class="box-body">
+                 <div class="table-responsive">
+                   <table class="table no-margin">
+                     <thead>
+                     <tr>
+                       <th>Date</th>
+                       <th>Title</th>
+                       <th>Source</th>
+                     </tr>
+                     </thead>
+                     <tbody>
 
-                @foreach($documents as $document)
-                  <div class="box-footer">
-                    <div class="box-comment">
-                      <div class="comment-text">
-                        <a href="/{{ $document->file }}" target="_blank">{{ $document->name }}</a>
-                      </div>
-                      <!-- /.comment-text -->
-                    </div>
-                    <!-- /.box-comment -->
+                      @foreach($documents as $document)
+                        <tr>
+                          <td>{{ $document->publication_date }}</td>
+                          <td><a href="/admin/documents/{{ $document->id }}">{{ $document->title }}</a></td>
+                          <td>{{ $document->source }}</td>
+                        </tr>
+                      @endforeach
+
+                      </tbody>
+                    </table>
                   </div>
-                @endforeach
-
-               </div>
-               <!-- /.box-body -->
+                </div>
+                <!-- /.box-body -->
                 <div class="box-footer text-center">
                   <a href="{{ route('admin.documents.index') }}" class="uppercase">View All Documents</a>
                 </div>
