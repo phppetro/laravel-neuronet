6b08ba8b435115138d234afc973b653be8b9a19e
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2019-08-01 10:34:00 +0200

    Added one event per day to the calendar for events longuer than a day

--- app/Http/Controllers/Admin/SystemCalendarController.php
+++ app/Http/Controllers/Admin/SystemCalendarController.php
@@ -7,30 +7,67 @@ use Carbon\Carbon;
 
 class SystemCalendarController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        $events = []; 
+        $events = [];
 
-        foreach (\App\Calendar::all() as $calendar) { 
-           $crudFieldValue = $calendar->getOriginal('date'); 
+        foreach (\App\Calendar::all() as $calendar) {
+
+           $start = new Carbon($calendar->start_date_and_time);
+           $end = new Carbon($calendar->end_date_and_time);
+           $diff = $start->diff($end)->days;
+           $count = $diff - 1;
+
+           $crudFieldValue_next = $crudFieldValue = $calendar->getOriginal('start_date_and_time');
 
            if (! $crudFieldValue) {
                continue;
            }
 
-           $eventLabel     = $calendar->title; 
-           $prefix         = ''; 
-           $suffix         = ''; 
-           $dataFieldValue = trim($prefix . " " . $eventLabel . " " . $suffix); 
-           $events[]       = [ 
-                'title' => $dataFieldValue, 
-                'start' => $crudFieldValue, 
+           $eventLabel     = $calendar->title;
+
+           $prefix         = 'Starts ';
+           $suffix         = '';
+           $dataFieldValue = trim($prefix . " " . $eventLabel . " " . $suffix);
+           $events[]       = [
+                'title' => $dataFieldValue,
+                'start' => $crudFieldValue,
                 'url'   => route('admin.calendars.edit', $calendar->id)
-           ]; 
-        } 
+           ];
+
+           $crudFieldValue_2 = new Carbon($crudFieldValue_next);
+           while($count > 0) {
+             $crudFieldValue_2 = $crudFieldValue_2->addDays(1);
+             $crudFieldValue_3 = $crudFieldValue_2->toDateString();
+             $events[]       = [
+                  'title' => $eventLabel,
+                  'start' => $crudFieldValue_3,
+                  'url'   => route('admin.calendars.edit', $calendar->id)
+             ];
+             $count--;
+           }
+        }
+
+        foreach (\App\Calendar::all() as $calendar) {
+           $crudFieldValue = $calendar->getOriginal('end_date_and_time');
+
+           if (! $crudFieldValue) {
+               continue;
+           }
+
+           $eventLabel     = $calendar->title;
+           $prefix         = 'Ends ';
+           $suffix         = '';
+           $dataFieldValue = trim($prefix . " " . $eventLabel . " " . $suffix);
+           $events[]       = [
+                'title' => $dataFieldValue,
+                'start' => $crudFieldValue,
+                'url'   => route('admin.calendars.edit', $calendar->id)
+           ];
+        }
 
 
-       return view('admin.calendar' , compact('events')); 
+       return view('admin.calendar' , compact('events'));
     }
 
 }
