0c243a9dd4fe8d070222305781a415be6a0f3a6d
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2019-08-01 15:57:05 +0200

    Changed Future Events module in the dashboard to display the start date of the event

--- resources/views/admin/dashboard2/calendar2.blade.php
+++ resources/views/admin/dashboard2/calendar2.blade.php
@@ -24,7 +24,7 @@
 
           @foreach($events as $event)
             <tr>
-              <td>{{ $event->date }}</td>
+              <td>{{ $event->start_date }}</td>
               <td><a href="/admin/calendars/{{ $event->id }}">{{ $event->title }}</a></td>
               <td>{{ $event->project->name }}</td>
               <td>{{ $event->location }}</td>
