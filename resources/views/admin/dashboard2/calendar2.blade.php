01424500ed1527af4787980240584c07b982d004
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2019-09-13 18:31:09 +0200

    Added projects to Event in the dashboard, now all selected projects will be visible and if no project is selected a message General will be displayed, I have also changed the title Future event for events

--- resources/views/admin/dashboard2/calendar2.blade.php
+++ resources/views/admin/dashboard2/calendar2.blade.php
@@ -1,6 +1,6 @@
 <div class="box box-purple">
   <div class="box-header with-border">
-    <h3 class="box-title">Future Events</h3>
+    <h3 class="box-title">Events</h3>
 
     <div class="box-tools pull-right">
       <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -16,16 +16,24 @@
         <tr>
           <th>Date</th>
           <th>Event</th>
+          <th>Projects</th>
           <th>Location</th>
         </tr>
         </thead>
         <tbody>
 
-
           @foreach($events as $event)
             <tr>
               <td>{{ $event->start_date }}</td>
               <td><a href="/admin/calendars/{{ $event->id }}">{{ $event->title }}</a></td>
+              <td>@if($event->projects->isNotEmpty())
+                    @foreach ($event->projects as $singleProjects)
+                        <span>{{ $singleProjects->name }}</span>
+                    @endforeach
+                  @else
+                    General
+                  @endif
+              </td>
               <td>{{ $event->location }}</td>
             </tr>
           @endforeach
