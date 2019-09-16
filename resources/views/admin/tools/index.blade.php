840d9630d36608dd1eb332dbc4761faf9fb6250e
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2019-09-16 16:12:38 +0200

    Fixed Index in tools, to show the Add the new button correctly

--- resources/views/admin/tools/index.blade.php
+++ resources/views/admin/tools/index.blade.php
@@ -2,27 +2,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3 class="page-title">@lang('global.tools.title')</h3>
-    @can('tool_create')
-    <p>
-        <a href="{{ route('admin.tools.create') }}" class="btn btn-success">@lang('global.app_add_new')</a>
-        <a href="#" class="btn btn-warning" style="margin-left:5px;" data-toggle="modal" data-target="#myModal">@lang('global.app_csvImport')</a>
-        @include('csvImport.modal', ['model' => 'Tool'])
-        
-    </p>
-    @endcan
+    <h3 class="page-title">
+      @lang('global.tools.title')
 
+    @can('tool_create')
+
+        <a href="{{ route('admin.tools.create') }}" class="btn btn-success">@lang('global.app_add_new')</a>
+        @can('tool_csv_import')
+          <a href="#" class="btn btn-warning" style="margin-left:5px;" data-toggle="modal" data-target="#myModal">@lang('global.app_csvImport')</a>
+          @include('csvImport.modal', ['model' => 'Tool'])
+        @endcan
+
+    @endcan
+    </h3>
+    @can('tool_perma_del')
     <p>
         <ul class="list-inline">
             <li><a href="{{ route('admin.tools.index') }}" style="{{ request('show_deleted') == 1 ? '' : 'font-weight: 700' }}">@lang('global.app_all')</a></li> |
             <li><a href="{{ route('admin.tools.index') }}?show_deleted=1" style="{{ request('show_deleted') == 1 ? 'font-weight: 700' : '' }}">@lang('global.app_trash')</a></li>
         </ul>
     </p>
-    
+    @endcan
 
     <div class="panel panel-default">
         <div class="panel-heading">
-            @lang('global.app_list')
+          <div class="btn-group">
+              <button type="button" class="btn btn-default btn-flat">Filter by project</button>
+              <button type="button" class="btn btn-default btn-flat dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                <span class="caret"></span>
+                <span class="sr-only">Toggle Dropdown</span>
+              </button>
+              <ul class="dropdown-menu" role="menu">
+                @foreach($projects as $project)
+                  @if($project->name == $project_name)
+                    <li><a href="/admin/tools/project/{{ $project->id }}"><b><u>{{ $project->name }}</u></b></a></li>
+                  @else
+                    <li><a href="/admin/tools/project/{{ $project->id }}">{{ $project->name }}</a></li>
+                  @endif
+                @endforeach
+              </ul>
+            </div>
+            @if($project_name)
+                <a class="btn btn-info" href="/admin/tools">Applied filter: "{{ $project_name }}" <u>Click here to remove it</u></a>
+            @endif
         </div>
 
         <div class="panel-body table-responsive">
@@ -52,13 +74,20 @@
     </div>
 @stop
 
-@section('javascript') 
+@section('javascript')
     <script>
         @can('tool_delete')
             @if ( request('show_deleted') != 1 ) window.route_mass_crud_entries_destroy = '{{ route('admin.tools.mass_destroy') }}'; @endif
         @endcan
         $(document).ready(function () {
-            window.dtDefaultOptions.ajax = '{!! route('admin.tools.index') !!}?show_deleted={{ request('show_deleted') }}';
+            var project_id='';
+            var currentURL = $(location).attr('href');
+            if (currentURL.indexOf("project") >= 0) {
+              var array = currentURL.split('/');
+              project_id = array[6];
+            }
+            window.dtDefaultOptions.ajax = '{!! route('admin.tools.index') !!}?show_deleted={{ request('show_deleted') }}&project_id=';
+            window.dtDefaultOptions.ajax+=project_id;
             window.dtDefaultOptions.columns = [@can('tool_delete')
                 @if ( request('show_deleted') != 1 )
                     {data: 'massDelete', name: 'id', searchable: false, sortable: false},
@@ -70,7 +99,6 @@
                 {data: 'description', name: 'description'},
                 {data: 'keywords', name: 'keywords'},
                 {data: 'link', name: 'link'},
-                
                 {data: 'actions', name: 'actions', searchable: false, sortable: false}
             ];
             processAjaxTables();
