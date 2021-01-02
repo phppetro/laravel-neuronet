4e093d0961e36b60603fbd26d1dfe9323aa5ba80
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2021-01-02 16:47:08 +0100

    Change dashboard distribution

--- resources/views/public_dashboard.blade.php
+++ resources/views/public_dashboard.blade.php
@@ -6,23 +6,22 @@
     	<div class="row">
   	    <div class="col-md-4">
       		@include('admin.dashboard2.activity2')
-      		@include('admin.dashboard2.contacts')
 			@include('admin.dashboard2.projects')
+            @include('admin.dashboard2.documents2')
+            @include('admin.dashboard2.tools')
+            @include('admin.dashboard2.calendar2')
       	</div>
 		<div class="col-md-8">
 			@include('admin.dashboard2.metrics4')
 		</div>
 		<div class="col-md-4">
-			@include('admin.dashboard2.publications2')
-			@include('admin.dashboard2.documents2')
-			@include('admin.dashboard2.calendar2')
-			@include('admin.dashboard2.tools')
-			@include('admin.dashboard2.asset_map')
+            @include('admin.dashboard2.asset_map')
+            @include('admin.dashboard2.decision_tool')
 		</div>
 		<div class="col-md-4">
 			@include('admin.dashboard2.schedule2')
+            @include('admin.dashboard2.publications2')
 			@include('admin.dashboard2.deliverables3')
-			@include('admin.dashboard2.decision_tool')
         </div>
      </div>
   </div>
