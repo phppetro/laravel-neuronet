b47e9858fafd1d1aa9cec824c65b5caa6517b8e1
ruslanpopelyshyn <ruslanpopelishin@gmail.com>
2021-01-30 19:19:29 +0100

    Put schedule module in dashboard in two columns

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,6 +11,7 @@
       	</div>
 		<div class="col-md-8">
 			@include('admin.dashboard2.metrics4')
+            @include('admin.dashboard2.schedule2')
 		</div>
 		<div class="col-md-4">
             @include('admin.dashboard2.asset_map')
@@ -20,7 +21,6 @@
 {{--            @include('admin.dashboard2.key_areas')--}}
 		</div>
 		<div class="col-md-4">
-			@include('admin.dashboard2.schedule2')
             @include('admin.dashboard2.publications2')
 			@include('admin.dashboard2.deliverables3')
             @include('admin.dashboard2.tools')
